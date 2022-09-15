35ad5f4ede0c7267666152eba4b0bd8d0d909f04
kmikonowicz <karolinamikonopwicz@gmail.com>
2022-09-15 20:44:07 +0200

    game2

--- maze/script.js
+++ maze/script.js
@@ -26,7 +26,6 @@ function makeWall(x, y, w, h, type = 'wall') {
     top:${y}%;
     position:absolute;
     box-shadow: rgba(0, 0, 0, 0.09) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px;
-
   `
   // dodajemy klasy do każdego diva
   wall.className = 'wall'
